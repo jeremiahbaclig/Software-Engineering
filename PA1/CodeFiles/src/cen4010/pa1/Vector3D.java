b1590364eb64362a88cb412f6dd778fade452935
Jeremiah Baclig <JeremiahBaclig@gmail.com>
2021-02-01 18:13:21 -0500

    Added comments.

--- PA1/CodeFiles/src/cen4010/pa1/Vector3D.java
+++ PA1/CodeFiles/src/cen4010/pa1/Vector3D.java
@@ -8,33 +8,42 @@ public class Vector3D {
 	private double z;
 	private final double HUNDREDTHS = 0.001;
 	
-	// Double wrapper class is immutable
+	/* 
+	 * Double wrapper class is immutable.
+	 * constructor that takes the x, y, and z coordinates, which should be doubles 
+	 */
 	public Vector3D(Double x, Double y, Double z) {  
 		this.x = x;
 		this.y = y;
 		this.z = z;
 	}
 	
-	public Vector3D scale(double f) {
+	// should multiply x, y, and z by a common factor f.
+	public Vector3D scale(double f) { 
 		return new Vector3D(this.x*f, this.y*f, this.z*f);
 	}
 	
+	// takes one Vector3D as an argument adds the corresponding coordinates to its own and produces a new Vector3D
 	public Vector3D add(Vector3D v) {
 		return new Vector3D(this.x+v.x, this.y+v.y, this.z+v.z);
 	}
 	
+	// subtract argument v's coordinates from the corresponding coordinates in "this" producing a new Vector3D object
 	public Vector3D subtract(Vector3D v) {
 		return new Vector3D(this.x-v.x, this.y-v.y, this.z-v.z);
 	}
 	
+	// shorthand for scale by -1
 	public Vector3D negate() {
 		return new Vector3D(this.x*-1, this.y*-1, this.z*-1);
 	}
 	
+	// Produce the dot product of "this" Vector3D and argument Vector3D v
 	public double dot(Vector3D v) {
 		return this.x*v.x + this.y*v.y + this.z*v.z;
 	}
 	
+	// returns the magnitude of a Vector3D
 	public double magnitude() {
 		return Math.sqrt(x*x + y*y + z*z);
 	}
@@ -43,6 +52,7 @@ public class Vector3D {
 		return String.format(null, null);  // "for reasonable output"?
 	}
 	
+	// implementation of equals - float and double arithmetic is not exact, thus, must allow for a tolerance
 	public boolean equals(Vector3D v) {
 		if(Math.abs(v.x-this.x) < HUNDREDTHS){
 			return true;  
