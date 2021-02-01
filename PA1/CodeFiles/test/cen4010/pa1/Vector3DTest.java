98437321f52c3d2e59ccd3fd025bb827333ef859
Jeremiah Baclig <JeremiahBaclig@gmail.com>
2021-02-01 18:26:42 -0500

    added skeleton unit test methods

--- PA1/CodeFiles/test/cen4010/pa1/Vector3DTest.java
+++ PA1/CodeFiles/test/cen4010/pa1/Vector3DTest.java
@@ -8,7 +8,48 @@ import src.cen4010.pa1.Vector3D;
 import static org.junit.jupiter.api.Assertions.*;
 
 class Vector3DTest {
-
+	
+	@Test
+	public void testToString() {
+		
+	}
+	
+	@Test
+	public void testEquals() {  // very basic equality test, self-equality, equality to a different object, tests varying only one coordinate at a time.
+		
+	}
+	
+	/*
+	 * very similar to testEquals, and needs a working equals to test it. checking the unit scale as 
+	 * returning (a copy of) the same vector, and scaling by some value to make test multiplying each coordinate
+	 * 
+	 */
+	@Test
+	public void testScale() {
+		
+	}
+	
+	@Test
+	public void testAdd() {  // simple tests
+		
+	}
+	
+	@Test
+	public void testSubtract() {  // simple tests
+		
+	}
+	
+	@Test
+	public void testNegate() {  // simple tests
+		
+	}
+	
+	@Test
+	public void testMagnitute() {  // simple tests
+		
+	}
+	
+	
     // WILL BE DELETED - just testing
     @Test
     public void testSampleMethod() {
