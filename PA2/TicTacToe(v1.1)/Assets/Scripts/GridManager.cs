53421adb2df5049676b5e18e09c6f0907f4e750a
Jeremiah Baclig <JeremiahBaclig@gmail.com>
2021-02-28 05:39:47 -0500

    adjusted sprite numbering

--- PA2/TicTacToe(v1.1)/Assets/Scripts/GridManager.cs
+++ PA2/TicTacToe(v1.1)/Assets/Scripts/GridManager.cs
@@ -1,7 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using System;
 
 public class GridManager : MonoBehaviour
 {
@@ -11,10 +10,12 @@ public class GridManager : MonoBehaviour
     public GameObject square;
     private SpriteRenderer rend;
 
-    bool unplayed = true;
     private int m = 3;
     private int n = 3;
     private int k = 3;
+    int[,] boardState;
+    public bool gameOver = false;
+    public int winner = 0;
     private int turn = 0;
     private const float FACTOR_SHAPE = 2.75F;
     private const float FACTOR_LINE = 1.5F;
@@ -23,6 +24,7 @@ public class GridManager : MonoBehaviour
     {
         Grid();
         Players();
+        SetBoardState(m, n);
     }
 
     void Update()
@@ -35,24 +37,52 @@ public class GridManager : MonoBehaviour
                 int index = PlayerTurn();
                 if (index % 2 == 1) // player X
                 {
-                    if (CastRay().name.Contains("1") || CastRay().name.Contains("0"))
+                    if (CastRay().name.Contains(","))
                     {
+                        string name = CastRay().name;
+
                         rend = CastRay().GetComponent<SpriteRenderer>();
-                        pos = GameObject.Find(CastRay().name).transform.position;
-                        GameObject.Find(CastRay().name).SetActive(false);
+                        pos = GameObject.Find(name).transform.position;
+                        GameObject.Find(name).SetActive(false);
+
+                        string[] coords = name.Split(',');
+                        boardState[Int32.Parse(coords[0]), Int32.Parse(coords[1])] = 1;
+
                         Instantiate(xButton, pos, Quaternion.identity);
                     }
                 }
                 else // player O
                 {
-                    if (CastRay().name.Contains("1") || CastRay().name.Contains("0"))
+                    if (CastRay().name.Contains(","))
                     {
+                        string name = CastRay().name;
+
                         rend = CastRay().GetComponent<SpriteRenderer>();
-                        pos = GameObject.Find(CastRay().name).transform.position;
-                        GameObject.Find(CastRay().name).SetActive(false);
+                        pos = GameObject.Find(name).transform.position;
+                        GameObject.Find(name).SetActive(false);
+
+                        string[] coords = name.Split(',');
+                        boardState[Int32.Parse(coords[0]), Int32.Parse(coords[1])] = -1;
+
                         Instantiate(oButton, pos, Quaternion.identity);
                     }
                 }
+            }
+        }
+        gameOver = GetBoardState();
+        if(gameOver == true)
+        {
+            if (winner == 1)
+            {
+                Debug.Log("X WINS");
+            }
+            else if(winner == -1)
+            {
+                Debug.Log("O WINS");
+            }
+            else if(winner == -99)
+            {
+                Debug.Log("TIE");
             }
         }
     }
@@ -77,16 +107,164 @@ public class GridManager : MonoBehaviour
 
     private void Players()
     {
-        for (int i = -1; i < m - 1; i++)
+        for (int i = 0; i < m; i++)
         {
-            for (int j = -1; j < n - 1; j++)
+            for (int j = 0; j < n; j++)
             {
-                square = Instantiate(square, new Vector3(i * FACTOR_SHAPE, j * FACTOR_SHAPE, 0), Quaternion.identity);
-                square.name = i.ToString() + j.ToString();
+                square = Instantiate(square, new Vector3((i-1) * FACTOR_SHAPE, (j-1) * FACTOR_SHAPE, 0), Quaternion.identity);
+                if (j == 0)
+                {
+                    square.name = i.ToString() + "," + (j + (n - 1)).ToString();
+                }
+                else if (j == n - 1)
+                {
+                    square.name = i.ToString() + "," + (j - (n - 1)).ToString();
+                }
+                else
+                {
+                    square.name = i.ToString() + "," + j.ToString();
+                }
                 rend = square.GetComponent<SpriteRenderer>();
                 rend.sortingOrder = -2;
             }
         }
+    }
+
+    private void SetBoardState(int rows, int cols)
+    {
+        boardState = new int[rows, cols];
+    }
+
+    private bool GetBoardState()
+    {
+        int counter = 0;
+        for (int i = 0; i < m; i++) // check rows
+        {
+            for(int j = 0; j < n; j++)
+            {
+                if (boardState[i, j] == -1)
+                {
+                    counter++;
+                }
+                else if (boardState[i, j] == 1)
+                {
+                    counter--;
+                }
+            }
+            if(counter == n)
+            {
+                winner = -1;
+                return true;
+            }
+            else if(counter == -n)
+            {
+                winner = 1;
+                return true;
+            }
+            else
+                counter = 0;
+        }
+
+        for (int i = 0; i < m; i++) // check cols
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (boardState[j, i] == -1)
+                {
+                    counter++;
+                }
+                else if (boardState[i, j] == 1)
+                {
+                    counter--;
+                }
+            }
+            if (counter == m) // need code for O win
+            {
+                winner = -1;
+                return true;
+            }
+            else if (counter == -m) 
+            {
+                winner = 1;
+                return true;
+            }
+            else
+                counter = 0;
+        }
+
+        for (int i = 0; i < m; i++) // check diag
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (i == j)
+                {
+                    if (boardState[i, j] == -1)
+                    {
+                        counter++;
+                    }
+                    else if (boardState[i, j] == 1)
+                    {
+                        counter--;
+                    }
+                }
+            }
+            if (counter == m) 
+            {
+                winner = -1;
+                return true;
+            }
+            else if (counter == -m)
+            {
+                winner = 1;
+                return true;
+            }
+            else
+                counter = 0;
+        }
+
+        for (int i = 0; i < m; i++) // check counter-diag
+        {
+            for (int j = n-1; j >= 0 ; j--)
+            {
+                if (boardState[i, j] == -1)
+                {
+                    counter++;
+                }
+                else if (boardState[i, j] == 1)
+                {
+                    counter--;
+                }
+            }
+            if (counter == m)
+            {
+                winner = -1;
+                return true;
+            }
+            else if (counter == -m)
+            {
+                winner = 1;
+                return true;
+            }
+            else
+                counter = 0;
+        }
+
+        for (int i = 0; i < m; i++) // check tie
+        {
+            for (int j = 0; j < n; j++)
+            {
+                if (boardState[i, j] != 0)
+                {
+                    counter++;
+                }
+            }
+        }
+        if(counter == (m * n) - 1)
+        {
+            winner = -99;
+        }
+
+        return false;
     }
 
     public int PlayerTurn()
@@ -102,7 +280,6 @@ public class GridManager : MonoBehaviour
         if (Physics.Raycast(ray, out hit, 100))
         {
             Debug.DrawLine(ray.origin, hit.point);
-            Debug.Log("Hit object: " + hit.collider.gameObject.name);
             return hit.collider.gameObject;
         }
         return null;
