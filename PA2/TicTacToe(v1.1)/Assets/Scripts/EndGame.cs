85193963168052901c109e280b56572be357ba1a
KymberleeS <sableskymberlee@yahoo.com>
2021-03-03 20:18:12 -0500

    completed timer ran out game scenario

---
+++ PA2/TicTacToe(v1.1)/Assets/Scripts/EndGame.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndGame : MonoBehaviour
+{
+   public void PlayAgain() {
+        SceneManager.LoadScene("GameScene");
+   }
+
+   public void ReturnToMenu() {
+        SceneManager.LoadScene("Menu");
+   }
+}
