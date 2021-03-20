e187d0cabb7751009b01abdf65d1e7b21e44b8a4
RuralYoung <ruralyoungiii@gmail.com>
2021-03-19 20:57:03 -0400

    Fixed the Diagonal Test

--- PA3/TicTacToe(v1.2)/Assets/Tests/TestSuite.cs
+++ PA3/TicTacToe(v1.2)/Assets/Tests/TestSuite.cs
@@ -77,5 +77,31 @@ namespace Tests
 
             yield return null;
         }
+
+        [UnityTest]
+        public IEnumerator TestWinDiagonal()
+        {
+            var game = new GameObject().AddComponent<GridManager>();
+
+            game.line = MonoBehaviour.Instantiate(Resources.Load<GameObject>("line"));
+            game.xButton = MonoBehaviour.Instantiate(Resources.Load<GameObject>("x"));
+            game.oButton = MonoBehaviour.Instantiate(Resources.Load<GameObject>("o"));
+            game.square = MonoBehaviour.Instantiate(Resources.Load<GameObject>("square"));
+            game.border = MonoBehaviour.Instantiate(Resources.Load<GameObject>("border"));
+
+            game.Start();
+
+            Vector3 pos1 = new Vector3((float)-2.8, (float)2.8, 0);
+            Vector3 pos2 = new Vector3(0, 0, 0);
+            Vector3 pos3 = new Vector3((float)2.8, (float)-2.8, 0);
+
+            game.PlayerX(pos1, "0,0");
+            game.PlayerX(pos2, "1,1");
+            game.PlayerX(pos3, "2,2");
+
+            Assert.IsTrue(game.CheckBoardState());
+
+            yield return null;
+        }
     }
 }
