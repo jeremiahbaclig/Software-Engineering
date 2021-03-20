b38caf81c784179f6acd47a9bad9d824da481138
RuralYoung <ruralyoungiii@gmail.com>
2021-03-19 20:42:51 -0400

    Fixed the Horizontal test

--- PA3/TicTacToe(v1.2)/Assets/Tests/TestSuite.cs
+++ PA3/TicTacToe(v1.2)/Assets/Tests/TestSuite.cs
@@ -38,7 +38,7 @@ namespace Tests
             game.border = MonoBehaviour.Instantiate(Resources.Load<GameObject>("border"));
 
             game.Start();
-
+            
             Vector3 pos1 = new Vector3((float)-2.8, (float)2.8, 0);
             Vector3 pos2 = new Vector3(0, (float)2.8, 0);
             Vector3 pos3 = new Vector3((float)2.8, (float)2.8, 0);
@@ -46,6 +46,32 @@ namespace Tests
             game.PlayerX(pos1, "0,0");
             game.PlayerX(pos2, "1,0");
             game.PlayerX(pos3, "2,0");
+
+            Assert.IsTrue(game.CheckBoardState());
+
+            yield return null;
+        }
+
+        [UnityTest]
+        public IEnumerator TestWinHorizontal()
+        {
+            var game = new GameObject().AddComponent<GridManager>();
+
+            game.line = MonoBehaviour.Instantiate(Resources.Load<GameObject>("line"));
+            game.xButton = MonoBehaviour.Instantiate(Resources.Load<GameObject>("x"));
+            game.oButton = MonoBehaviour.Instantiate(Resources.Load<GameObject>("o"));
+            game.square = MonoBehaviour.Instantiate(Resources.Load<GameObject>("square"));
+            game.border = MonoBehaviour.Instantiate(Resources.Load<GameObject>("border"));
+
+            game.Start();
+
+            Vector3 pos1 = new Vector3((float)-2.8, (float)2.8, 0);
+            Vector3 pos2 = new Vector3(0, (float)2.8, 0);
+            Vector3 pos3 = new Vector3((float)2.8, (float)2.8, 0);
+
+            game.PlayerX(pos1, "0,0");
+            game.PlayerX(pos2, "0,1");
+            game.PlayerX(pos3, "0,2");
 
             Assert.IsTrue(game.CheckBoardState());
 
